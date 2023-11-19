275b4ffeba430acb06a903ba2e77184eb898b6db
Cristoffer Östberg <cristoffer_ostberg85@hotmail.com>
2023-11-19 17:40:05 +0100

    To 55:38, create repository layer.
    Second commit, changed User + Role class to more fit MongoDB-style,
    
     - not able to test full program yet if works since the SecurityConfig-class is out of style/not working yet.
     Need the full code/program of rest of classes to be able to test.
     Securityconfig-class directly copied from repo right now since as of information that is the one working (the one in video when done isnt working - the one in the repo is updated without further information passed)
    
     Rest of program as written/described in video tut.

--- src/main/java/how/to/unknownkoderspringsecurity/UnknownKoderSpringSecurityApplication.java
+++ src/main/java/how/to/unknownkoderspringsecurity/UnknownKoderSpringSecurityApplication.java
@@ -1,7 +1,19 @@
 package how.to.unknownkoderspringsecurity;
 
+import how.to.unknownkoderspringsecurity.model.Role;
+import how.to.unknownkoderspringsecurity.model.User;
+import how.to.unknownkoderspringsecurity.repository.RoleRepository;
+import how.to.unknownkoderspringsecurity.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.springframework.util.ClassUtils.isPresent;
 
 @SpringBootApplication
 public class UnknownKoderSpringSecurityApplication {
@@ -10,4 +22,19 @@ public class UnknownKoderSpringSecurityApplication {
         SpringApplication.run(UnknownKoderSpringSecurityApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder){
+        return args ->{
+            if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+            Role adminRole = roleRepository.save(new Role("ADMIN"));
+                             roleRepository.save(new Role("USER"));
+
+                             Set<Role> roles = new HashSet<>();
+                             roles.add(adminRole);
+
+                             User admin = new User(1, "admin", passwordEncoder.encode("password"), roles);
+
+                             userRepository.save(admin);
+        };
+    }
 }
